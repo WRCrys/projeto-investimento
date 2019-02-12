4441e69fcc840ba63155ed24cfbcf96daa557e04
Crystyano Almeida <winnerbyjesus@gmail.com>
2019-02-12 18:43:00 -0200

    front end of the dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -20,7 +20,7 @@ class DashboardController extends Controller
     }
     
     public function index(){
-        return 'We are in index';
+        return view('user.dashboard');
     }
 
     public function auth(Request $request){
---
+++ resources/views/templates/master.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Investindo</title>
+    @yield('css-view')
+    <link rel="stylesheet" href="{{asset('css/stylesheet.css')}}">
+    <link href="https://fonts.googleapis.com/css?family=Fredoka+One" rel="stylesheet" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+</head>
+<body>
+    @include('templates.menu-lateral')
+    @yield('conteudo-view')
+    @yield('js-view')
+</body>
+</html>
